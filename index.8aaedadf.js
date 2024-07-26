5e4b197b47881fc418b7f13c1e4d6c5088cf09ba
SvitlanaRamanauskas <ramanauskassvs@gmail.com>
2024-07-26 11:36:37 +0300

    make build

---
+++ index.8aaedadf.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".header"),a=document.querySelector(".header__menu--link");a.addEventListener("hashchange",function(){"#menu"===a.hash?e.classList.add("page__menu"):e.classList.remove("page__menu")});
+//# sourceMappingURL=index.8aaedadf.js.map
